69a7fb2a5938c38461fda5f49bfab77c069ba716
furuya <furuya003@gmail.com>
2021-12-19 12:45:16 +0900

    regster industryall organization to DataFrame (#1)

--- main.py
+++ main.py
@@ -55,32 +55,39 @@ def main():
     industryall_organization_one = [
         [td.string for td in tr] for tr in industryall_organization_all
     ]
-    # print(industryall_organization_one)
+    # print(industryall_organization_one[0][0])
 
     # 企業一覧のURL取得
     corporate_all = [
         tr for tr in soup.select("#industry-group-table > tbody > tr > td:nth-child(5) > a")
     ]
     corporate_one = [tag_a.get("href") for tag_a in corporate_all]
-    # print(corporate_one)
+    print(len(corporate_one))
 
     # ホームページのURL取得
     homepage_all = [
         tr for tr in soup.select("#industry-group-table > tbody > tr > td:nth-child(6) > a")
     ]
     homepage_one = [tag_a.get("href") for tag_a in homepage_all]
-    # print(homepage_one)
+    print(len(homepage_one))
+
+    import pandas as pd
+
+    df = pd.DataFrame(industryall_organization_one, columns=table_head)
+    for i, company in enumerate(df["企業一覧"]):
+        if company is None:
+            continue
+        else:
+            df.at[i, df.columns[4]] = corporate_one.pop(0)
+    for i, homepage in enumerate(df["ホームページ"]):
+        if homepage is None:
+            continue
+        else:
+            df.at[i, df.columns[5]] = homepage_one.pop(0)
+    df.to_csv(
+        "pandas.csv",
+    )
 
 
 if __name__ == "__main__":
     main()
-
-    # import pandas as pd
-
-    # df = pd.DataFrame(
-    #     {"名前": ["田中", "山田", "高橋"], "役割": ["営業部長", "広報部", "技術責任者"], "身長": [178, 173, 169]}
-    # )
-    # print(df)
-    # print(df.dtypes)
-
-    # print(df.columns)  # 列ラベルの確認(辞書型のkeyが列ラベル）
